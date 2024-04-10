b9cab5d3d67b12f9622f5c23001d04b351ddd640
MouadAgn <aoughanemouad1@gmail.com>
2024-04-10 14:03:04 +0200

    Migrations | APP corse error

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,14 @@
 import Login from './Login'
 import { Routes, Route} from "react-router-dom"
 import Home from './Home'
+import Planning from './Planning'
 
 function App() {
  
   return (
     <div>
         <Routes>
+          <Route path="/planning" element={<Planning />}> </Route>
           <Route path="/" element={<Home />}> </Route>
           <Route path="/login" element={<Login/>}></Route>
         </Routes>
