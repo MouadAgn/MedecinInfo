745d354d9aaa5ab66d0c7f150da86a90d1b2a158
MouadAgn <aoughanemouad1@gmail.com>
2024-04-12 14:31:35 +0200

    UPDATE

--- FrontEnd/src/Patient.jsx
+++ FrontEnd/src/Patient.jsx
@@ -7,29 +7,25 @@ import Footer from './Footer.jsx';
 function Patient() {
     const [loading, setLoading] = useState(true); // Ajout de l'état pour le chargement
     const [appointments, setAppointments] = useState([]);
-    const [, setSelectedPatient] = useState(null); // État pour stocker le patient sélectionné pour la suppression
+   // État pour stocker le patient sélectionné pour la suppression
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('http://127.0.0.1:8000/api/patients');
                 const patientsData = await response.json();
-                const numberOfPatients = patientsData.length;
-
+                
                 const allAppointments = [];
 
-                for (let i = 1; i <= numberOfPatients; i++) {
-                    const response = await fetch(`http://127.0.0.1:8000/api/appointments/patient/${i}`);
-                    const appointmentsData = await response.json();
+                for (const patient of patientsData) {
+                    const patientId = patient.id;
+                    const appointmentResponse = await fetch(`http://127.0.0.1:8000/api/appointments/patient/${patientId}`);
+                    const appointmentsData = await appointmentResponse.json();
 
-                    const existingAppointments = allAppointments.find(appointments => appointments.patientId === i);
-                    if (!existingAppointments) {
-                        allAppointments.push({ patientId: i, appointments: appointmentsData });
-                    }
+                    allAppointments.push({ patient: patient, appointments: appointmentsData });
                 }
+                
                 setAppointments(allAppointments);
-                console.log(allAppointments);
-                // console.log(appointments);
                 setLoading(false); // Mettre à jour l'état du chargement une fois les données chargées
             } catch (error) {
                 console.error('Erreur lors de la récupération des données', error);
@@ -65,19 +61,16 @@ function Patient() {
                         </tr>
                     </thead>
                     <tbody>
-                        {allAppointments.map(patient => (
+                        {appointments.map(patient => (
                             <tr key={patient.appointments.patient.id}>
-                                {/* <p>{patient.appointments.appointments.date}</p> */}
                                 <td>{patient.appointments.patient.id}</td>
                                 <td>{patient.appointments.patient.name}</td>
                                 <td>{patient.appointments.patient.dob}</td>
                                 <td>{patient.appointments.patient.gender === 1 ? 'Homme' : 'Femme'}</td>
-                                <td>+33{patient.appointments.patient.phone}</td>
+                                <td>{patient.appointments.patient.phone}</td>
                                 <td>{patient.appointments.appointments.date ? `${patient.appointments.appointments.date} à ${patient.appointments.appointments.time}` : 'Pas de RDV'}</td>
-                                <td>
-                                    <button style={{backgroundColor: 'blue'}}><Link to={`/treatments/patient/${patient.appointments.patient.id}`}>Traitements</Link></button>&nbsp;&nbsp;
-                                    <button style={{backgroundColor: 'red'}}><Link to={`/appointments/patient/${patient.appointments.patient.id}`}>Rendez-vous</Link></button>
-                                </td>
+
+                               
                             </tr>
                         ))}
                     </tbody>
